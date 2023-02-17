7af817907b70ba8312615650aa038edab771df69
Evan Richard <evan@ironfish.network>
2023-02-17 15:43:25 -0500

    Update exportAccount.ts
    use yup to note exported spending key is nullable

--- ironfish/src/rpc/routes/wallet/exportAccount.ts
+++ ironfish/src/rpc/routes/wallet/exportAccount.ts
@@ -30,7 +30,7 @@ export const ExportAccountResponseSchema: yup.ObjectSchema<ExportAccountResponse
     account: yup
       .object({
         name: yup.string().defined(),
-        spendingKey: yup.string().defined(),
+        spendingKey: yup.string().defined().nullable(),
         viewKey: yup.string().defined(),
         incomingViewKey: yup.string().defined(),
         outgoingViewKey: yup.string().defined(),
