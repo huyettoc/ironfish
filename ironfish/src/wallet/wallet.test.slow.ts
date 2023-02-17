775f70e7a55f9813477438d26b2ed44732311cc7
Evan Richard <evan@ironfish.network>
2023-02-17 13:13:32 -0500

    yarn lint --fix

--- ironfish/src/wallet/wallet.test.slow.ts
+++ ironfish/src/wallet/wallet.test.slow.ts
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Asset, generateKey } from '@ironfish/rust-nodejs'
-import { Assert } from '../assert'
 import { Target } from '../primitives/target'
 import {
   createNodeTest,
