ddfb8543b98ac3788a13e2ff2bfa9ac8d2cb250b
Evan Richard <evan@ironfish.network>
2023-02-17 16:21:39 -0500

    more renaming

--- ironfish/src/rpc/routes/wallet/importViewAccount.ts
+++ ironfish/src/rpc/routes/wallet/importViewAccount.ts
@@ -5,43 +5,41 @@ import * as yup from 'yup'
 import { AccountImport } from '../../../wallet/account'
 import { ApiNamespace, router } from '../router'
 
-export type ImportViewingAccountRequest = {
+export type ImportViewAccountRequest = {
   account: AccountImport
   rescan?: boolean
 }
 
-export type ImportViewingAccountResponse = {
+export type ImportViewAccountResponse = {
   name: string
   isDefaultAccount: boolean
 }
 
-export const ImportViewingAccountRequestSchema: yup.ObjectSchema<ImportViewingAccountRequest> =
-  yup
-    .object({
-      rescan: yup.boolean().optional().default(true),
-      account: yup
-        .object({
-          name: yup.string().defined(),
-          viewingKey: yup.string().defined(),
-          incomingViewingKey: yup.string().defined(),
-          outgoingViewingKey: yup.string().defined(),
-          version: yup.number().defined(),
-        })
-        .defined(),
-    })
-    .defined()
+export const ImportViewAccountRequestSchema: yup.ObjectSchema<ImportViewAccountRequest> = yup
+  .object({
+    rescan: yup.boolean().optional().default(true),
+    account: yup
+      .object({
+        name: yup.string().defined(),
+        viewKey: yup.string().defined(),
+        incomingViewKey: yup.string().defined(),
+        outgoingViewKey: yup.string().defined(),
+        version: yup.number().defined(),
+      })
+      .defined(),
+  })
+  .defined()
 
-export const ImportViewingAccountResponseSchema: yup.ObjectSchema<ImportViewingAccountResponse> =
-  yup
-    .object({
-      name: yup.string().defined(),
-      isDefaultAccount: yup.boolean().defined(),
-    })
-    .defined()
+export const ImportViewAccountResponseSchema: yup.ObjectSchema<ImportViewAccountResponse> = yup
+  .object({
+    name: yup.string().defined(),
+    isDefaultAccount: yup.boolean().defined(),
+  })
+  .defined()
 
-router.register<typeof ImportViewingAccountRequestSchema, ImportViewingAccountResponse>(
+router.register<typeof ImportViewAccountRequestSchema, ImportViewAccountResponse>(
   `${ApiNamespace.wallet}/importAccount`,
-  ImportViewingAccountRequestSchema,
+  ImportViewAccountRequestSchema,
   async (request, node): Promise<void> => {
     const account = await node.wallet.importAccount(request.data.account)
 
